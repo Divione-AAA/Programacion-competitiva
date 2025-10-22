554f7b05aa60261a63cd63927db5b77a8975dfdc
Divione-AAA <hectordavid55555211@gmail.com>
2025-10-22 00:07:04 -0400

    commit

--- Ejercicios/TRES PARTES DE UN ARREGLO/main.cpp
+++ Ejercicios/TRES PARTES DE UN ARREGLO/main.cpp
@@ -1,51 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define loop(i,n) for(int i=0;i<n;i++)
-#define loopi(i,n) for(int i=0;i<=n;i++)
-#define lup(i,x,n) for(int i=x;i<=n;i++)
-#define loup(i,x,n) for(int i=x;i<n;i++)
-#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ll long long
-#define int long long
-#define double long double
-#define endl '\n'
-#define over(i,a) for(auto i: a)
-#define F first
-#define S second
-#define MP make_pair
-#define PB push_back
-#define PF push_front
-#define INS insert
-#define sortt(a) sort(a.begin(),a.end())
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-const int INF=4294967296;
-const int _INF=-4294967296;
-const double pi=3.1415926535;
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
 
-int n;
-vector<int> d;
-vector<pair<int,int>> l,r;
-
-int32_t main(){
-    fast
-    //freopen("file.in", "r", stdin);freopen("file.out", "w", stdout);
-
-    cin>>n;
-    d.resize(n+1);
-    l.resize(n+1);
-    r.resize(n+1);
-    d[0]=0;
-
-    loopi(i,n){
-        cin>>d[i];
-        l[i].first=d[i]+l[i-1].first;
-        l[i].second=i;
+    int i = 0, j = n - 1;
+    long long left_sum = a[i], right_sum = a[j];
+    long long ans = 0;
+    while (i < j) {
+        if (left_sum == right_sum) {
+            ans = left_sum;       
+            ++i;
+            left_sum += a[i];
+        } else if (left_sum < right_sum) {
+            ++i;
+            left_sum += a[i];
+        } else {
+            --j;
+            right_sum += a[j];
+        }
     }
-    r[n].first=d[n];
-    for(int i=n-1;i>=1;i--){
-        r[i].first=d[i]+r[i+1].first;
-        r[i].second=i;
-    }
-    return 0;
-}
+    cout<<ans<<endl;
+return 0;}
